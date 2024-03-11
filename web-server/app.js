aa57982161d1cb816f0094b3d75ea3285867940b
sailesh <saileshkhadka1@gmail.com>
2024-03-11 22:49:24 +0400

    code changes and updated docker secret and username for github ci/cd

--- web-server/app.js
+++ web-server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Web-server!');
+  res.send('Hello from kube-server!');
 });
 
 app.listen(port, () => {
